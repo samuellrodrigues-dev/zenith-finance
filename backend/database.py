de2907e2722e2ba936c688a8748b0413a446809c
Samuel Rodrigues Pereira Junior <samuel@MacBook-Air-de-Samuel.local>
2025-12-30 10:19:36 -0300

    Adicionando coluna date no banco

--- backend/database.py
+++ backend/database.py
@@ -1,22 +1,23 @@
-# backend/database.py
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# Cria o arquivo do banco de dados (finance.db)
-DATABASE_URL = "sqlite:///./finance.db"
+# Pega o banco da nuvem (Render) ou usa local se não tiver
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./finance.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Define como é uma "Transação" no nosso banco
 class Transaction(Base):
-    __tablename__ = "transactions"
+    # MUDAMOS O NOME PARA FORÇAR A CRIAÇÃO DE UMA TABELA NOVA COM A COLUNA DATE
+    __tablename__ = "transactions_v2"
 
     id = Column(Integer, primary_key=True, index=True)
-    description = Column(String, index=True) # Ex: Almoço, Uber
-    amount = Column(Float)                   # Ex: -50.00, 1000.00
-    category = Column(String)                # Ex: Alimentação, Transporte
-    type = Column(String)                    # Ex: despesa, receita
+    description = Column(String(255), index=True)
+    amount = Column(Float)
+    category = Column(String(255))
+    type = Column(String(255))
+    date = Column(String(50)) # <--- AQUI ESTÁ A NOVIDADE!
